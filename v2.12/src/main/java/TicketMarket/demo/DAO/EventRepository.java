8b76a7e28cde6b329fd5efca2fbdbb3be627c08f
gilGamliel <gilpsp3g@gmail.com>
2024-12-03 12:31:29 +0200

    Delete v2.12 directory

--- v2.12/src/main/java/TicketMarket/demo/DAO/EventRepository.java
+++
@@ -1,8 +0,0 @@
-package TicketMarket.demo.DAO;
-
-import TicketMarket.demo.Entity.Event;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface EventRepository extends JpaRepository<Event, Integer> , EventCustomRepository {
-}
